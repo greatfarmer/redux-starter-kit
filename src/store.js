52932db60a29712ef182fcafad59f55aafb781ec
Seonghun <lutis107@gmail.com>
2018-08-29 22:57:01 +0900

    Import 'promiseMiddleware'

--- src/store.js
+++ src/store.js
@@ -2,14 +2,21 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
 import { createLogger } from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
+
+// 사용하지 않으므로 주석 처리
+// import ReduxThunk from 'redux-thunk';
+
+import promiseMiddleware from 'redux-promise-middleware';
 
 /*
   로그 미들웨어를 만들 때 설정을 커스터마이징할 수 있습니다.
   https://github.com/evgenyrodionov/redux-logger#options
 */
 const logger = createLogger();
+const pm = promiseMiddleware({
+  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
+const store = createStore(modules, applyMiddleware(logger, pm));
 
 export default store;

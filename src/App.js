bbf8d1650a138ce69d4737b661117ca34d4daf3f
Seonghun <lutis107@gmail.com>
2018-08-29 22:30:45 +0900

    Request using action

--- src/App.js
+++ src/App.js
@@ -2,32 +2,66 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-import axios from 'axios';
+import * as postActions from './modules/post';
 
 class App extends Component {
+  loadData = async () => {
+    const { PostActions, number } = this.props;
+    try {
+      const response = await PostActions.getPost(number);
+      console.log(response);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts/1')
-      .then(response => console.log(response));
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // 이전 number와 현재 number가 다르면 요청을 시작합니다.
+    if(this.props.number !== prevProps.number) {
+      this.loadData();
+    }
   }
 
   render() {
-      const { CounterActions, number } = this.props;
-      
-      return (
-          <div>
-              <h1>{number}</h1>
-              <button onClick={CounterActions.incrementAsync}>+</button>
-              <button onClick={CounterActions.decrementAsync}>-</button>
-          </div>
-      );
+    const { CounterActions, number, post, error, loading } = this.props;
+    
+    return (
+        <div>
+          <h1>{number}</h1>
+          {
+            loading
+              ? (<h2>로딩중...</h2>)
+              : (
+                error
+                  ? (<h2>오류 발생!</h2>)
+                  : (
+                    <div>
+                      <h2>{post.title}</h2>
+                      <p>{post.body}</p>
+                    </div>
+                  )
+              )
+          }
+          <button onClick={CounterActions.increment}>+</button>
+          <button onClick={CounterActions.decrement}>-</button>
+        </div>
+    );
   }
 }
 
 export default connect(
   (state) => ({
-      number: state.counter
+      number: state.counter,
+      post: state.post.data,
+      loading: state.post.pending,
+      error: state.post.error
   }),
   (dispatch) => ({
-      CounterActions: bindActionCreators(counterActions, dispatch)
+      CounterActions: bindActionCreators(counterActions, dispatch),
+      postActions: bindActionCreators(postActions, dispatch)
   })
 )(App);
